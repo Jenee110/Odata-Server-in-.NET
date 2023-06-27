92398fae7a01c382ff819c78094281080083209f
Jenee110 <94671763+Jenee110@users.noreply.github.com>
2023-06-27 17:15:05 +0530

    Create NoteAPPContext.cs

---
+++ NoteAPPContext.cs
@@ -0,0 +1,17 @@
+
+using Microsoft.EntityFrameworkCore;
+using ODataTutorial.Entities;
+
+namespace ODataTutorial.EntityFramework
+{
+    public class NoteAppContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; } = default;
+
+        public DbSet<Interaction> Interactions { get; set; } = default;
+        public NoteAppContext(DbContextOptions<NoteAppContext> options) : base(options)
+        {
+
+        }
+    }
+}
